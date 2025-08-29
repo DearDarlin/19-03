9158143bff79c2fa1ceb3964fcc474a3f194e830
DearDarlin <darinakovalenko78@gmail.com>
2025-08-29 23:29:27 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+class Progtam
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 56, 89, 1, 67, 0, 3, 23 };
+        var ascending = numbers.OrderBy(n => Summa(n));
+        var descending = numbers.OrderByDescending(n => Summa(n));
+
+        Console.WriteLine("Array: " + string.Join(", ", numbers));
+        Console.WriteLine("Ascending: " + string.Join(", ", ascending));
+        Console.WriteLine("Descending: " + string.Join(", ", descending));
+    }
+    static int Summa(int n)
+    {
+        return n.ToString().Sum(c => c - '0');
+    }
+}
